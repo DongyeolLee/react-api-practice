3d25adda96ba52877295bc043956b3645d822f05
DongyeolLee <dongyeol8@gmail.com>
2018-07-09 16:11:01 +0900

    setting and viewTemplate

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ViewerTemplate from "./components/ViewerTemplate/ViewerTemplate";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ViewerTemplate/>
+      </div>
+    );
+  }
+}
+
+export default App;
