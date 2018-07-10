321771809fb3009482b0862019bc63ee460dc192
DongyeolLee <dongyeol8@gmail.com>
2018-07-10 10:33:13 +0900

    SpaceNavigator.js and scss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import ViewerTemplate from "./components/ViewerTemplate/ViewerTemplate";
+import SpaceNavigator from "./components/SpaceNavigator/SpaceNavigator";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <ViewerTemplate/>
+        <ViewerTemplate
+          spaceNavigator={<SpaceNavigator/>}
+        />
       </div>
     );
   }
